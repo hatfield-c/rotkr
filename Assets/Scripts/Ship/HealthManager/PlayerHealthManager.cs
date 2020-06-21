2f5c7620dc99ae4b5ac9c04007c1e8ab88098f3b
Cody Hatfield <hatfield.coders@gmail.com>
2020-06-21 09:31:28 -0400

    [ROTKR-16] Added HunkManager and some basic hunks to the ship

---
+++ Assets/Scripts/Ship/HealthManager/PlayerHealthManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : AbstractHealthManager
+{
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+    }
+
+    void Update(){
+        
+    }
+}
---
+++ Assets/Scripts/Ship/Hunk/Hunk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnJointBreak(float breakForce){
+        this.transform.parent = null;
+    }
+}
