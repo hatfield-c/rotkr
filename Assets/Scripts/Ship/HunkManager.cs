2587e54f1ae15a868805df8d847d873368dcb081
Cody Hatfield <hatfield.coders@gmail.com>
2020-06-21 09:31:28 -0400

    [ROTKR-16] Added HunkManager and some basic hunks to the ship

---
+++ Assets/Scripts/Ship/HunkManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class HunkManager
+{
+    public GameObject jointOrigin;
+    public Transform hunkBlueprint;
+    public float breakForce = 0.0f;
+    public float breakTorque = 0.0f;
+    public bool jointCollision = false;
+    public bool enablePreprocessing = true;
+
+    protected List<Hunk> hunkList;
+
+    public void Init(List<HunkData> hunkData){
+        if(hunkData != null){
+            this.hunkList = this.loadHunkData(hunkData);
+        } else {
+            this.hunkList = this.createHunks();
+        }
+    }
+
+    protected List<Hunk> loadHunkData(List<HunkData> hunkData){
+        return null;
+    }
+
+    protected List<Hunk> createHunks(){
+        List<Hunk> hunks = new List<Hunk>();
+        
+        foreach(Transform hunkTransform in this.hunkBlueprint){
+            GameObject hunkObject = hunkTransform.gameObject;
+
+            FixedJoint hunkJoint = hunkObject.AddComponent<FixedJoint>();
+            //hunkJoint.connectedBody = jointOrigin.rigidbody;
+            hunkJoint.breakForce = this.breakForce;
+            hunkJoint.breakTorque = this.breakTorque;
+            hunkJoint.enableCollision = this.jointCollision;
+            hunkJoint.enablePreprocessing = this.enablePreprocessing;
+
+        }
+
+        return hunks;
+    }
+
+}
