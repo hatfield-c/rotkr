ce52169ba451a186af5fe06966fd55a5aaf62713
KevinDCo <co.kevindaniel@gmail.com>
2020-06-25 22:47:50 -0500

    [ROTKR-22] Made great progress with drowning behaviour. Need to finish up rat health system

--- Assets/Scripts/Actors/RatHealthSystem.cs
+++ Assets/Scripts/Actors/RatHealthSystem.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RatHealthSystem : MonoBehaviour, IDamageable, IRepairable
 {
     #region variables
     [SerializeField] float health = 100f;
     public float Health => health;
+    // How many seconds the rat needs to drown for to TakeDamage
+    public float DrowningPeriod = 1f;
+    // How much damage the rat takes every drowning cycle
+    public float DrowningDamage = 10f;
+    Sequence currentSequence = null;
     #endregion
 
     #region logic
@@ -22,7 +29,8 @@ public class RatHealthSystem : MonoBehaviour, IDamageable, IRepairable
     #region public functions
     public void TakeDamage(float value)
     {
-        health += value;
+        health -= value;
+        //TODO: health death logic
     }
     public void Repair(float value)
     {
@@ -32,6 +40,28 @@ public class RatHealthSystem : MonoBehaviour, IDamageable, IRepairable
     {
         return health;
     }
+    public void Drown()
+    {
+        if (currentSequence != null)
+            return;
+        Debug.Log("passed return");
+        Sequence sequence = DOTween.Sequence();
+        sequence.InsertCallback(DrowningPeriod, () => {
+            TakeDamage(DrowningDamage);
+            Drown();
+            currentSequence = null;
+        });
+        sequence.SetAutoKill(false);
+        sequence.Pause();
+        currentSequence.Kill();
+        currentSequence = sequence;
+        currentSequence.Play();
+    }
+
+    public void StopDrowning()
+    {
+        currentSequence.Kill();
+    }
     #endregion
 
     #region private functions
@@ -39,7 +69,5 @@ public class RatHealthSystem : MonoBehaviour, IDamageable, IRepairable
     {
         health = value;
     }
-
-
     #endregion
 }
