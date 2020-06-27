f0275d8466c279961a3427dd1a79ae3820cb7514
Cody Hatfield <hatfield.coders@gmail.com>
2020-06-27 07:16:41 -0400

    [ROTKR-27] Replaced transform floatation method with force-based method

--- Assets/Scripts/WaterFloat.cs
+++ Assets/Scripts/WaterFloat.cs
@@ -7,131 +7,33 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class WaterFloat : MonoBehaviour
 {
-    //public properties
-    public float AirDrag = 1;
-    public float WaterDrag = 10;
-    public bool AffectDirection = true;
-    public bool AttachToSurface = false;
+    public Transform waterLevel;
     public Transform[] FloatPoints;
-
-    //used components
+    public FloatDeadzone deadzone;
+    public BuoyancyParameters buoyancyParameters;
+ 
     protected Rigidbody Rigidbody;
-    //protected Waves Waves;
+    protected BuoyancyManager buoyManager;
+    protected BuoyantForce bf;
 
-    //water line
-    protected float WaterLine;
-    protected Vector3[] WaterLinePoints;
-
-    //help Vectors
-    protected Vector3 smoothVectorRotation;
-    protected Vector3 TargetUp;
-    protected Vector3 centerOffset;
-
-    public Vector3 Center { get { return transform.position + centerOffset; } }
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        //get components
-        //Waves = FindObjectOfType<Waves>();
-        Rigidbody = GetComponent<Rigidbody>();
-        //Rigidbody.useGravity = false;
-
-        //compute center
-        WaterLinePoints = new Vector3[FloatPoints.Length];
-        for (int i = 0; i < FloatPoints.Length; i++)
-            WaterLinePoints[i] = FloatPoints[i].position;
-        centerOffset = PhysicsHelper.GetCenter(WaterLinePoints) - transform.position;
-
+    void Awake() {
+        this.Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //default water surface
-        var newWaterLine = 0f;
-        var pointUnderWater = false;
-
-        //set WaterLinePoints and WaterLine
-        for (int i = 0; i < FloatPoints.Length; i++)
-        {
-            //height
-            WaterLinePoints[i] = FloatPoints[i].position;
-            //WaterLinePoints[i].y = Waves.GetHeight(FloatPoints[i].position);
-            WaterLinePoints[i].y = 0f;
-            newWaterLine += WaterLinePoints[i].y / FloatPoints.Length;
-            if (WaterLinePoints[i].y > FloatPoints[i].position.y)
-                pointUnderWater = true;
-        }
-
-        var waterLineDelta = newWaterLine - WaterLine;
-        WaterLine = newWaterLine;
-
-        //compute up vector
-        TargetUp = PhysicsHelper.GetNormal(WaterLinePoints);
-
-        //gravity
-        var gravity = Physics.gravity;
-        Rigidbody.drag = AirDrag;
-        if (WaterLine > Center.y)
-        {
-            Rigidbody.drag = WaterDrag;
-            //under water
-            if (AttachToSurface)
-            {
-                //attach to water surface
-                Rigidbody.position = new Vector3(Rigidbody.position.x, WaterLine - centerOffset.y, Rigidbody.position.z);
-            }
-            else
-            {
-                //go up
-                gravity = AffectDirection ? TargetUp * -Physics.gravity.y : -Physics.gravity;
-                transform.Translate(Vector3.up * waterLineDelta * 0.9f);
-            }
-        }
-        Rigidbody.AddForce(gravity * Mathf.Clamp(Mathf.Abs(WaterLine - Center.y), 0, 1));
-
-        //rotation
-        if (pointUnderWater)
-        {
-            //attach to water surface
-            TargetUp = Vector3.SmoothDamp(transform.up, TargetUp, ref smoothVectorRotation, 0.2f);
-            Rigidbody.rotation = Quaternion.FromToRotation(transform.up, TargetUp) * Rigidbody.rotation;
-        }
-
+    void Start(){
+        this.buoyManager = new BuoyancyManager(
+            this.Rigidbody, 
+            this.waterLevel, 
+            this.deadzone, 
+            this.buoyancyParameters
+        );
     }
 
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.green;
-    //    if (FloatPoints == null)
-    //        return;
+    void FixedUpdate(){
 
-    //    for (int i = 0; i < FloatPoints.Length; i++)
-    //    {
-    //        if (FloatPoints[i] == null)
-    //            continue;
+        this.bf = this.buoyManager.update(this.FloatPoints);
 
-    //        if (Waves != null)
-    //        {
+        this.Rigidbody.AddForceAtPosition(bf.force, bf.position);
 
-    //            //draw cube
-    //            Gizmos.color = Color.red;
-    //            Gizmos.DrawCube(WaterLinePoints[i], Vector3.one * 0.3f);
-    //        }
-
-    //        //draw sphere
-    //        Gizmos.color = Color.green;
-    //        Gizmos.DrawSphere(FloatPoints[i].position, 0.1f);
-
-    //    }
-
-    //    //draw center
-    //    if (Application.isPlaying)
-    //    {
-    //        Gizmos.color = Color.red;
-    //        Gizmos.DrawCube(new Vector3(Center.x, WaterLine, Center.z), Vector3.one * 1f);
-    //        Gizmos.DrawRay(new Vector3(Center.x, WaterLine, Center.z), TargetUp * 1f);
-    //    }
-    //}
+    }
 }
