b323e3843eb9d6e47d951d1cac139400e373f473
KevinDCo <co.kevindaniel@gmail.com>
2020-06-27 19:14:02 -0500

    [ROTKR-32] started working on GameManager and included state machine setup

---
+++ Assets/Scripts/StateManagement/IGameState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGameState
+{
+    void Execute();
+    void Cancel();
+
+}
+public abstract class AGameState : IGameState
+{
+    public Action ExecuteComplete;
+    public Action CancelComplete;
+
+    public abstract void Execute();
+    public abstract void Cancel();
+}
