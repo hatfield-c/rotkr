8e28dc51bf14881af6cde95d3ae649a90f8eb32d
KevinDCo <co.kevindaniel@gmail.com>
2020-06-29 08:12:50 -0500

    [ROTKR-32] More progress on the state machine

---
+++ Assets/Scripts/StateManagement/WyeState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeOfWye { None, CollectionChamber, Spillway };
+public class WyeState : AGameState
+{
+    public WyeData Data;
+    public WyeState(WyeData data)
+    {
+        Data = data;
+    }
+
+    #region references
+    #endregion
+
+    #region handlers
+    #endregion
+
+    #region logic
+    #endregion
+
+    public override void Execute()
+    {
+        ExecuteComplete?.Invoke();
+    }
+    public override void Cancel()
+    {
+        CancelComplete?.Invoke();
+    }
+}
+
+public class WyeData
+{
+    public TypeOfWye WyeType;
+}
