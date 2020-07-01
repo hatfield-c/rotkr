4c4cf1bf86b8014986b6a1c1c08d41c4534075b1
Cody Hatfield <hatfield.coders@gmail.com>
2020-07-01 04:44:17 -0400

    [ROTKR-31] Revision of buoyancy script

--- Assets/Scripts/Ship/Physics/BuoyancyParameters.cs
+++ Assets/Scripts/Ship/Physics/BuoyancyParameters.cs
@@ -6,11 +6,21 @@ using UnityEngine;
 public class BuoyancyParameters
 {
     public float force = 9.8f;
-    public float forceRatio = 1f;
-    public float forceBias = 1f;
-    public float threshold = 0f;
+    public float damping = 0.0f;
+    public float maxDepth = 1.0f;
 
-    public float calculate(float depth){
-        return (forceRatio / (Mathf.Exp( (forceRatio - (depth - threshold)) * forceBias))) * this.force;
+    public float buoyantForce(float depth){
+        return this.force * Mathf.Exp(depth * this.damping);
+    }
+
+    public float hammerForce(float stopDistance, float velocity, float mass){
+        float acceleration = Mathf.Pow(velocity, 2) / (2 * stopDistance);
+        return mass * acceleration;
+    }
+
+    public float stopDistance(float depth, float velocity, float mass){
+        float acceleration = this.force / mass;
+        float timeToStop = velocity / acceleration;
+        return (velocity * timeToStop) - (0.5f * acceleration * Mathf.Pow(timeToStop, 2));
     }
 }
