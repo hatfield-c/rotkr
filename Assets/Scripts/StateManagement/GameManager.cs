2a95c08e3dda3e1b316ae3dc53d4a60cc2e0cad6
KevinDCo <co.kevindaniel@gmail.com>
2020-07-11 00:02:43 -0500

    [ROTKR-32] Game Manager created, basic scene loading implemented with the creation of states. Also put in simple LevelLoader for level transitions

--- Assets/Scripts/StateManagement/GameManager.cs
+++ Assets/Scripts/StateManagement/GameManager.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameManager : Singleton<GameManager>
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
 {
-    #region references
-    [SerializeField] MainMenuUI MainMenuUI;
-    MainMenuState mainMenuState;
-    IGameState currentState;
-    #endregion
-
+    public static GameManager Instance { get; private set; }
     #region state variables
     int wyesCompleted = 0;
     #endregion
+
+    #region references
+    [SerializeField] MainMenuUI MainMenuUI = null;
+    MainMenuState mainMenuState;
+    IGameState currentState;
+
+    // Singleton references
+    public LevelLoader levelLoader;
+    #endregion
+
+    #region handlers
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // update references
+        levelLoader = GameObject.FindObjectOfType<LevelLoader>();
+    }
+    #endregion
+
+    #region logic
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }        
+    }
+
+
     void Start()
     {
         mainMenuState = new MainMenuState(MainMenuUI);
@@ -35,10 +71,10 @@ public class GameManager : Singleton<GameManager>
         currentState.Execute();
     }
 
-    void Update()
-    {
-        
-    }
+    void Update() { }
+    #endregion
+
+    #region private functions
     void ChangeState(IGameState state)
     {
         Debug.Log($"<color=red>Changed from {currentState} to {state}.</color>");
@@ -67,4 +103,5 @@ public class GameManager : Singleton<GameManager>
         ChangeState(wye);
         currentState.Execute();
     }
+    #endregion
 }
