7d8f5e1ad5fff9563e54b31fa152fc3ddaeef6da
Cody Hatfield <hatfield.coders@gmail.com>
2020-07-12 14:00:48 -0400

    [ROTKR-17] Cannons have been added

---
+++ Assets/Scripts/Ship/EquipmentManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EquipmentManager
+{
+    public GameObject[] equipmentList;
+    public Transform ammunitionStorage;
+
+    public void Init(InputMaster controls){
+        foreach(GameObject equipmentObject in this.equipmentList){
+            IEquipment equipment = equipmentObject.GetComponent<IEquipment>();
+            equipment.registerInput(controls);
+        }
+
+        this.ammunitionStorage.parent = null;
+    }
+}
---
+++ Assets/Scripts/Utility/KatinTimer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatinTimer
+{
+    protected float length;
+    protected float elapsedTime;
+    protected bool active = false;
+
+    public delegate void Callback();
+    protected Callback callback;
+
+    public KatinTimer(){ }
+
+    public void Init(float length, Callback callback){
+        this.length = length;
+        this.callback = callback;
+        this.elapsedTime = 0;
+        this.active = true;
+    }
+
+    public void update(){
+        if(!this.active){
+            return;
+        }
+
+        this.elapsedTime += Time.deltaTime;
+
+        if(this.elapsedTime >= this.length){
+            this.alarm();
+        }
+    }
+
+    public void alarm(){
+        this.active = false;
+        this.elapsedTime = 0;
+        this.length = 0;
+        this.callback();
+        this.callback = this.empty;
+    }
+
+    public void empty(){ }
+}
