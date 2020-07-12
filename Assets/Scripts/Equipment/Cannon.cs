405f3951b30e1c25873ade17a44203fb2e17ef74
Cody Hatfield <hatfield.coders@gmail.com>
2020-07-12 14:00:48 -0400

    [ROTKR-17] Cannons have been added

---
+++ Assets/Scripts/Equipment/Cannon.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour, IEquipment
+{
+    public CannonBall projectile;
+    public Transform shootOrigin;
+    public float projectileAngle = 2.736f;
+    public float projectileAcceleration = 10f;
+    public float reloadTime = 1.5f;
+    public float maxDelay = 0.5f;
+    public float minDelay = 0f;
+
+    protected bool loaded = true;
+    protected KatinTimer fuseTimer;
+    protected KatinTimer reloadTimer = new KatinTimer();
+
+    void Start() {
+        this.fuseTimer = new KatinTimer();
+        this.reloadTimer = new KatinTimer();
+        this.shootOrigin.eulerAngles = new Vector3(
+            -this.projectileAngle,
+            this.shootOrigin.eulerAngles.y,
+            this.shootOrigin.eulerAngles.z
+        );
+    }
+
+    void Update() {
+        this.fuseTimer.update();
+        this.reloadTimer.update();
+    }
+
+    public void fire(){
+        this.projectile.activate();
+
+        this.projectile.getRigidbody().velocity = Vector3.zero;
+        this.projectile.transform.position = this.shootOrigin.position;
+        this.projectile.transform.rotation = this.shootOrigin.rotation;
+
+        this.projectile.getRigidbody().AddForce(
+            this.projectile.transform.forward * this.projectileAcceleration,
+            ForceMode.VelocityChange
+        );
+    }
+
+    public void lightFuse(){
+        if(!this.loaded){
+            return;
+        }
+
+        float fuseTime = Random.Range(this.minDelay, this.maxDelay);
+        this.fuseTimer.Init(fuseTime, this.fire);
+        this.reloadTimer.Init(this.reloadTime, this.reload);
+
+        this.loaded = false;
+    }
+
+    public void reload(){
+        this.loaded = true;
+    }
+
+    public void registerInput(InputMaster controls){
+        controls.Player.Shoot.performed += context => this.lightFuse();
+    }
+}
---
+++ Assets/Scripts/Equipment/CannonBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    public float despawnTime = 7f;
+
+    protected KatinTimer despawnTimer;
+    protected Rigidbody physicsBody;
+
+    void Awake()
+    {
+        this.physicsBody = this.GetComponent<Rigidbody>();
+        this.despawnTimer = new KatinTimer();
+    }
+
+    void FixedUpdate(){
+        this.despawnTimer.update();
+    }
+
+    public void activate(){
+        this.gameObject.SetActive(true);
+        this.despawnTimer.Init(this.despawnTime, this.deactivate);
+    }
+
+    public void deactivate(){
+        this.physicsBody.velocity = Vector3.zero;
+        this.gameObject.SetActive(false);
+    }
+
+    public Rigidbody getRigidbody(){
+        return this.physicsBody;
+    }
+}
