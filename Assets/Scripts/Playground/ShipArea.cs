a80dc167af7d174be48fdf726a0d8caa586eb72f
Cody Hatfield <hatfield.coders@gmail.com>
2020-07-22 23:56:37 -0400

    [ROTKR-40] Added basic machine learning scene

---
+++ Assets/Scripts/Playground/ShipArea.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using TMPro;
+
+public class ShipArea : MonoBehaviour {
+    public ShipAgent shipAgent;
+    public TextMeshPro rewardText;
+
+    public ShipTarget target;
+
+    void Start(){
+
+    }
+
+    void Update() {
+        this.rewardText.text = this.shipAgent.GetCumulativeReward().ToString();
+    }
+
+    public void ResetArea(){
+        this.target.resetSelf();
+    }
+}
