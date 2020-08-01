5abb68a239ce53b351e6b39ef535094eeebb5648
KevinDCo <co.kevindaniel@gmail.com>
2020-08-01 03:26:26 -0500

    [ROTKR-50][ROTKR-51] building AViews and used it to implement the Repair Menu logic

---
+++ Assets/Scripts/UI/AView.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+[RequireComponent(typeof(CanvasGroup))]
+[RequireComponent(typeof(CanvasRenderer))]
+[RequireComponent(typeof(GraphicRaycaster))]
+public abstract class AView : MonoBehaviour, IView
+{
+    public enum ViewTransition { None, Fade };
+    public ViewTransition Transition = ViewTransition.Fade;
+    Sequence currentViewSequence = null;
+
+    #region references
+    CanvasGroup CanvasGroup;
+    #endregion
+
+    #region view variables
+    public bool HideOnStart = true;
+    public float fadeDuration = .5f;
+    #endregion
+
+    protected void Awake()
+    {
+        CanvasGroup = GetComponent<CanvasGroup>();
+    }
+    protected void Start()
+    {
+        if (HideOnStart)
+            InstantHide();
+    }
+
+    public virtual void Show()
+    {
+        currentViewSequence.Kill();
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.AppendCallback(() => 
+        {
+            CanvasGroup.interactable = false;
+            CanvasGroup.blocksRaycasts = false;
+        });
+
+        switch (Transition)
+        {
+            case ViewTransition.None:
+                break;
+            case ViewTransition.Fade:
+                sequence.Append(CanvasGroup.DOFade(1, fadeDuration));
+                break;
+        }
+
+        sequence.AppendCallback(() =>
+        {
+            CanvasGroup.interactable = true;
+            CanvasGroup.blocksRaycasts = true;
+        });
+
+        sequence.SetAutoKill(false);
+        sequence.Pause();
+        currentViewSequence = sequence;
+        currentViewSequence.Play();
+    }
+    public virtual void Hide()
+    {
+        currentViewSequence.Kill();
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.AppendCallback(() =>
+        {
+            CanvasGroup.interactable = false;
+            CanvasGroup.blocksRaycasts = false;
+        });
+
+        switch (Transition)
+        {
+            case ViewTransition.None:
+                break;
+            case ViewTransition.Fade:
+                sequence.Append(CanvasGroup.DOFade(0, fadeDuration));
+                break;
+        }
+
+        sequence.SetAutoKill(false);
+        sequence.Pause();
+        currentViewSequence = sequence;
+        currentViewSequence.Play();
+    }
+
+    void InstantHide()
+    {
+        CanvasGroup.interactable = false;
+        CanvasGroup.blocksRaycasts = false;
+        CanvasGroup.alpha = 0;
+    }
+}
---
+++ Assets/Scripts/UI/RepairMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RepairMenu : AView
+{
+    #region references
+    [SerializeField] Button BTN_Confirm = null;
+    #endregion
+
+    new void Awake() { base.Awake(); }
+    new void Start() { base.Start(); }
+    public void Init(Action callback = null)
+    {
+        if (callback != null)
+        {
+            BTN_Confirm.onClick.AddListener(() =>
+            {
+                callback.Invoke();
+                BTN_Confirm.onClick.RemoveAllListeners();
+                BTN_Confirm.interactable = false;
+            });
+        }
+    }
+    public override void Show()
+    {
+        base.Show();
+    }
+    public override void Hide()
+    {
+        base.Hide();
+    }
+}
