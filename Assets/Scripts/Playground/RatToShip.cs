37419b9ad0acac6606142b4ecad091bc72ecec16
Cody Hatfield <hatfield.coders@gmail.com>
2020-08-02 13:11:41 -0400

    [ROTKR-47] Rats can now stick to the deck of the ship will falling off

---
+++ Assets/Scripts/Playground/RatToShip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatToShip : MonoBehaviour
+{
+    public Transform ship;
+    public Rigidbody ratbody;
+    public float breakForce = 10f;
+
+    void Start()
+    {
+        this.ratbody = this.gameObject.GetComponent<Rigidbody>();
+        this.ratbody.isKinematic = true;
+        this.ratbody.useGravity = false;
+
+        this.transform.parent = this.ship;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+        float force = collision.impulse.magnitude / Time.fixedDeltaTime;
+
+        Debug.Log($"flag: {force}");
+
+        if(force >= this.breakForce){
+            this.transform.parent = null;
+            this.ratbody.isKinematic = false;
+            this.ratbody.useGravity = true;
+        }
+    }
+}
