36346c61546731fe819afa2163ffbbed4a479466
Cody Hatfield <hatfield.coders@gmail.com>
2020-08-06 16:18:06 -0400

    [ROTKR-52] Refactored rat-sticking-to-deck code to be more modular

---
+++ Assets/Scripts/Actors/Rat/StateManagement/GroundData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundData
+{
+    bool isGrounded;
+    GameObject deckUnderfoot;
+
+    public GroundData(bool isGrounded, GameObject deckUnderfoot){
+        this.isGrounded = isGrounded;
+        this.deckUnderfoot = deckUnderfoot;
+    }
+
+    public bool IsGrounded(){
+        return this.isGrounded;
+    }
+
+    public GameObject GetDeck(){
+        return this.deckUnderfoot;
+    }
+}
