e07bc75d56db8b89fabeffadcf43047dd5fce7f9
KevinDCo <co.kevindaniel@gmail.com>
2020-08-24 13:22:57 -0500

    [ROTKR-64] Some progress starting on Upgrade system

--- Assets/Scripts/StateManagement/Data/ShipData.cs
+++ Assets/Scripts/StateManagement/Data/ShipData.cs
@@ -7,10 +7,12 @@ public class ShipData
         HunkDatum = new List<HunkData>();
         RatDatum = new List<RatData>();
         ScrapData = new ScrapData(3);
+        UpgradeData = new UpgradeData();
     }
     public List<HunkData> HunkDatum = null;
     public List<RatData> RatDatum = null;
     public ScrapData ScrapData = null;
+    public UpgradeData UpgradeData = null;
     int maxRatCount = 5;
 
     public int GetMaxRatCount()
---
+++ Assets/Scripts/StateManagement/Data/UpgradeData.cs
@@ -0,0 +1,9 @@
+﻿public class UpgradeData
+{
+    public UpgradeData()
+    {
+
+    }
+    int hardenedGlueLevel = 0;
+    int reloadSpeedLevel = 0;
+}
